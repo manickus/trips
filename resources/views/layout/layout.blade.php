a777663c7a22f0566c535fea61f2eda43aa2cc4b
Dawid Monka <dawid.monka@hotmail.com>
2017-12-28 20:56:19 +0100

    added meta desctription

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="desctription" content="TripStory.pl - Anonimowe historie z imprez, oraz ciekawych życiowych doświadczeń">
+
+    <meta name="tags" content="Impreza, Anonimowe historie, Alkohol, Seks, Marihuana, Narkotyki">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
