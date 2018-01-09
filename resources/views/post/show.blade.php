24b12dfd75d6a6b7345cc4cb2602bbf17fa19199
Dawid Monka <dawid.monka@hotmail.com>
2018-01-09 06:39:14 +0100

    fast hotfix

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -2,9 +2,5 @@
 
 @section('content')
 	@include('partials.post')
-	<div style="display: flex;
-    width: 90%;
-    margin: auto;">
-	<a href="https://www.ceneo.pl/Konsole_do_gier#pid=16123&crid=153178&cid=24878" rel="nofollow"><img src="https://image2.ceneo.pl/data/banners/banner_40.jpg"></img></a>
-	</div>
+
 @endsection
