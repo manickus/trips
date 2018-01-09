1b9f39dc9043e64b40232310647410ee3fdf967e
Dawid Monka <dawid.monka@hotmail.com>
2018-01-09 06:32:04 +0100

    fast hotfix

--- resources/views/homepage/index.blade.php
+++ resources/views/homepage/index.blade.php
@@ -2,7 +2,23 @@
 
 @section('content')
 	@foreach ($posts as $post)
+
 		@include('partials.post')
+		@if($loop->index == 0)
+		<div style="display: flex;
+    width: 90%;
+    margin: auto;">
+	<a href="https://www.ceneo.pl/Konsole_do_gier#pid=16123&crid=153178&cid=24878" rel="nofollow"><img src="https://image2.ceneo.pl/data/banners/banner_40.jpg"></img></a>
+	</div>
+		@endif
+
+		@if($loop->index == 4)
+		<div style="display: flex;
+    width: 90%;
+    margin: auto;">
+    <a href="https://www.ceneo.pl/Telefony_komorkowe?utm_source=PP&utm_medium=Swieta&utm_campaign=CPC#pid=16123&crid=153181&cid=24878" rel="nofollow"><img src="https://image2.ceneo.pl/data/banners/banner_1611.jpg"></img></a>
+    </div>
+		@endif
 	@endforeach
 	{{ $posts->links() }}
 @endsection
